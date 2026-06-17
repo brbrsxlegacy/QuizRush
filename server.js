c1036789e7989fef66f47d752903957f15c1c022
brbrsxlegacy <torullu61@gmail.com>
2026-06-17 15:39:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static("public"));
-
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 function cleanJson(text) {
